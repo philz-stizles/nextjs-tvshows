8ff2ff08c14c394d47aca584137b846ef292721d
philz-stizles <theophilusighalo@gmail.com>
2021-04-03 00:13:40 +0100

    init server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const PORT = parseInt(process.env.PORT) || 3000
+
+const app = next({dev})
+const handler = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.all('*', (req, res) => {
+    return handler(req, res)
+  })
+
+  server.listen(PORT, error => {
+    if(error) {
+      throw error
+    }
+
+    console.log(`Server is ready`)
+  })
+})
+
